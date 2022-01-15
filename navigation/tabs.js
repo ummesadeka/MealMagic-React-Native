b9545271417c6f55e7afa4182a4a81f33dea4c4f
ummesadeka <sadekaahmed53@gmail.com>
2022-01-15 22:27:09 +0600

    project and screen setup

---
+++ navigation/tabs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { 
+    Text,
+    View
+} from 'react-native';
+
+const tabs = () => {
+    return (
+        <View>
+            <Text>Tabssss</Text>
+        </View>
+    )
+}
+
+export default tabs;
---
+++ screens/OrderDelivery.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { 
+    Text,
+    View
+} from 'react-native';
+
+const OrderDelivery = () => {
+    return (
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    )
+}
+
+export default OrderDelivery;
